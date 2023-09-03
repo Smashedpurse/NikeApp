8afeca87f4c8b75218b8830046918e0e21cfd6f4
Ivan Rubio <smashedpurse@gmail.com>
2023-09-02 22:12:25 -0600

    development of the structure

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main className="relative">
+      nav 1
+      <section className="xl:padding-1 wide:padding-r padding-b">Hero</section>
+      <section className="padding">Popular Product</section>
+      <section className="padding">SuperQuality</section>
+      <section className="padding-x py-10">Services</section>
+      <section className="padding">SpecialOffers</section>
+      <section className="padding bg-pale-blue">CustomerReviews</section>
+      <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+      <section className="padding-x padding-t bg-black">Footer</section>
+    </main>
+  );
+};
+
+export default App;
